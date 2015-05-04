e9181fbb745f110e5d7eebfafa0f4521dd79142a
aryaastha <astyarry@gmail.com>
2015-05-04 11:55:40 +0530

    few more analytics

--- sql/analytics.sql
+++ sql/analytics.sql
@@ -20,3 +20,21 @@ Select item_id,DATE(invoice_time) From `invoice_items`, `invoices` WHERE invoice
 
 --- Shop wise Total revenue
 Select shop_id, sum((price-cost_price)*invoice_items.quantity) From `invoice_items`, `invoices`, `owner_items` WHERE invoices.invoice_id = invoice_items.invoice_id and invoice_items.item_id = owner_items.item_id and shop_id IN (SELECT shop_id FROM `shops` natural join `owner` where owner_id = 1) group by shop_id; 
+-----------------------------------------------------------------------------
+[x] Area graph for revenue / profit for give range (weeks, months, years)
+[x] Peak/Crest Points for each Product vs Revenue
+[x] Peak/Crest Points for each Product vs Profit
+Predictions, this is bought with this, wallmart-ish
+  
+  
+
+**Peak/Crest Points for each Product vs Revenue
+**Peak/Crest Points for each Product vs Profit
+	SELECT * from `invoices`, (SELECT invoice_id,invoice_items.item_id,(invoice_items.price-owner_items.cost_price)*invoice_items.quantity as profit, invoice_items.price*invoice_items.quantity as revenue from `invoice_items`, `owner_items` where invoice_items.item_id = owner_items.item_id) as table2 where table2.invoice_id = invoices.invoice_id; 
+
+
+**group by yearly
+	SELECT shop_id, year(invoices.invoice_time),sum(invoice_amount),sum(profit) from `invoices`, (SELECT invoice_id,invoice_items.item_id,(invoice_items.price-owner_items.cost_price)*invoice_items.quantity as profit from `invoice_items`, `owner_items` where invoice_items.item_id = owner_items.item_id) as table2 where table2.invoice_id = invoices.invoice_id group by shop_id,year(invoice_time),  having shop_id in(SELECT shop_id from `shops` where owner_id = 1);
+
+**group by monthly
+	SELECT shop_id, year(invoices.invoice_time),month(invoices.invoice_time), sum(invoice_amount),sum(profit) from `invoices`, (SELECT invoice_id,invoice_items.item_id,(invoice_items.price-owner_items.cost_price)*invoice_items.quantity as profit from `invoice_items`, `owner_items` where invoice_items.item_id = owner_items.item_id) as table2 where table2.invoice_id = invoices.invoice_id group by shop_id,year(invoice_time), month(invoice_time)  having shop_id in(SELECT shop_id from `shops` where owner_id = 1);
